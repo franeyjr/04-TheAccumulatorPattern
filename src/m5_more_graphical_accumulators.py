f9543c93f6057432a0c8175a88614043fc6897f5
franeyjr <franeyjr@rose-hulman.edu>
2018-12-06 00:06:41 -0500

    DONE

--- src/m5_more_graphical_accumulators.py
+++ src/m5_more_graphical_accumulators.py
@@ -142,24 +142,24 @@ def run_test_draw_circles_from_rectangle():
     ###########################################################################
     # -------------------------------------------------------------------------
     window1 = rg.RoseWindow(750,550, 'Test 1 DRAW_CIRCLES_FROM_RECTANGLE')
-    rect1 = rg.Rectangle(rg.Point(400,250),rg.Point(440,325))
-    rect1.outline_thickness = 5
-    rect1.fill_color = 'green'
-    rect1.outline_color = 'black'
-    draw_circles_from_rectangle(4,5,rect1,window1)
-    rect2 = rg.Rectangle(rg.Point(600,400),rg.Point(500,450))
-    rect2.outline_thickness = 3
-    rect2.fill_color = 'blue'
-    rect2.outline_color = 'red'
-    draw_circles_from_rectangle((8,3,rect2,window1))
+    rect1 = rg.Rectangle(rg.Point(50,50),rg.Point(150,350))
+    rect1.outline_thickness = 3
+    rect1.fill_color = 'blue'
+    rect1.outline_color = 'yellow'
+    draw_circles_from_rectangle(3,6,rect1,window1)
+    rect2 = rg.Rectangle(rg.Point(250,250),rg.Point(475,575))
+    rect2.outline_thickness = 5
+    rect2.fill_color = 'pink'
+    rect2.outline_color = 'purple'
+    draw_circles_from_rectangle(7,7,rect2,window1)
     window1.close_on_mouse_click()
 
     window2 = rg.RoseWindow(620,380, 'TEST 2 DRAW_CIRCLES_FROM_RECTANGLE')
-    rect2 = rg.Rectangle(rg.Point(375,330),rg.Point(350,280))
-    rect2.outline_thickness = 5
-    rect2.fill_color = 'yellow'
-    rect2.outline_color = 'brown'
-    draw_circles_from_rectangle(6,10,rect2,window2)
+    rect2 = rg.Rectangle(rg.Point(250,150),rg.Point(400,400))
+    rect2.outline_thickness = 7
+    rect2.fill_color = 'green'
+    rect2.outline_color = 'yellow'
+    draw_circles_from_rectangle(8,3,rect2,window2)
     window2.close_on_mouse_click()
 
 
